7df1befb6c9d9f4d66ec8a0522a500b6a0b16cca
Jan Jorissen <janjorissen@gmail.com>
2015-12-17 17:45:18 +0100

    fixed dividend table

--- app/client/components/modules/DividendModule.js
+++ app/client/components/modules/DividendModule.js
@@ -4,16 +4,23 @@ import FixedDataTable  from 'fixed-data-table';
 import {Table, Column, Cell} from 'fixed-data-table';
 import { updatePortfolioDividends } from '../../../shared/helpers/stocks';
 import { createDividendTableData } from '../../../shared/helpers/tables';
+import {round} from '../../../shared/helpers/formatting';
 
 const DividendCell = ({rowIndex, data, col, ...props}) => (
   <Cell {...props}>
-   $ {data[rowIndex][col]}
+    $ {data[rowIndex][col]}
   </Cell>
 );
 
 const AmountCell = ({rowIndex, data, col, ...props}) => (
   <Cell {...props}>
     {data[rowIndex][col]} x
+  </Cell>
+);
+
+const TotalAmountCell = ({rowIndex, data, col, ...props}) => (
+  <Cell {...props}>
+    $ {round(data[rowIndex]['amount'] * data[rowIndex]['price'])}
   </Cell>
 );
 
@@ -42,7 +49,7 @@ class DividendModule extends React.Component {
         zipCode: '3001',
         email: 'janjorissen@gmail.com',
         date: new Date(),
-      },{
+      }, {
         firstName: 'Jan',
         lastName: 'Jorissen',
         city: 'Berlin',
@@ -56,52 +63,71 @@ class DividendModule extends React.Component {
 
   render() {
 
-    if (!this.props.user.userData || !this.props.user.userData.portfolio || !this.props.historical) return (<Grid style={{'textAlign':'center','padding':'20px'}}>
-      <div className="loader"></div>
-    </Grid>);
+
+
+    if (!this.props.user.userData || !this.props.user.userData.portfolio || !this.props.historical) return (
+      <Grid style={{'textAlign':'center','padding':'20px'}}>
+        <div className="loader"></div>
+      </Grid>);
 
     let portfolio = this.props.user.stockPortfolio;
+
+    let stockEntries = portfolio.collectionList;
+
+    let dividends = stockEntries.reduce((prev, curr) => {
+      if (curr.total_dividends) prev += curr.total_dividends;
+      return prev;
+    }, 0);
+
+    dividends = round(dividends);
+
     updatePortfolioDividends(portfolio, this.props.historical);
     let divs = createDividendTableData(portfolio, this.props.historical);
 
-
     return (
-      <Grid>
-        <Table
-          rowHeight={40}
-          headerHeight={40}
-          rowsCount={divs.length}
-          width={1100}
-          maxHeight={500}
-          {...this.props}>
-          <Column
-            header={<Cell>Ticker</Cell>}
-            cell={<TextCell data={divs} col="ticker" />}
-            fixed={true}
-            width={100}
-          />
-          <Column
-            header={<Cell>Amount</Cell>}
-            cell={<AmountCell data={divs} col="amount" />}
-            fixed={true}
-            width={100}
-          />
-          <Column
-            header={<Cell>Dividend</Cell>}
-            cell={<DividendCell data={divs} col="price" />}
-            fixed={true}
-            width={100}
-          />
-          <Column
-            header={<Cell>Date</Cell>}
-            cell={<DateCell data={divs} col="date" />}
-            fixed={true}
-            width={300}
-          />
-
-
-        </Table>
-      </Grid>
+      <div>
+        <Grid>
+          <Table
+            rowHeight={40}
+            headerHeight={40}
+            rowsCount={divs.length}
+            width={800}
+            maxHeight={500}
+            {...this.props}>
+            <Column
+              header={<Cell>Ticker</Cell>}
+              cell={<TextCell data={divs} col="ticker" />}
+              fixed={true}
+              width={100}
+            />
+            <Column
+              header={<Cell>Amount</Cell>}
+              cell={<AmountCell data={divs} col="amount" />}
+              fixed={true}
+              width={100}
+            />
+            <Column
+              header={<Cell>Dividend</Cell>}
+              cell={<DividendCell data={divs} col="price" />}
+              fixed={true}
+              width={100}
+            />
+            <Column
+              header={<Cell>Date</Cell>}
+              cell={<DateCell data={divs} col="date" />}
+              fixed={true}
+              width={400}
+            />
+            <Column
+              header={<Cell>Total</Cell>}
+              cell={<TotalAmountCell data={divs} col="" />}
+              fixed={true}
+              width={100}
+            />
+          </Table>
+          <p>Total Dividends Collected: {dividends}</p>
+        </Grid>
+      </div>
     );
   }
 }
--- app/shared/helpers/tables.js
+++ app/shared/helpers/tables.js
@@ -2,15 +2,27 @@
 import {sortByKey} from './sorting';
 
 export function createDividendTableData(portfolio, historical){
-  let divvies = [];
+
+  let divvies = {};
+  let total = 0;
+
   portfolio.allStockEntries.forEach(entry =>{
-    if(entry.receivedDividends)   entry.receivedDividends.forEach(div =>{
-      div.ticker = entry.ticker;
-      div.amount = entry.amount;
-      divvies.push(div);
-    })
+    if(entry.receivedDividends){
+      entry.receivedDividends.forEach(orig_div =>{
+        let div = Object.assign({},orig_div);
+        div.ticker = entry.ticker;
+        div.amount = entry.amount;
+        total += (entry.amount * orig_div.price);
+        if(!divvies[orig_div.date.format()]) {
+          divvies[orig_div.date.format()] = div;
+        }else{
+          divvies[orig_div.date.format()].amount += entry.amount;
+        }
+      })
+    }
   });
 
-  divvies = sortByKey(divvies,'date');
-  return divvies;
+  let divs = Object.keys(divvies).map(key => divvies[key]);
+  return sortByKey(divs,'date');
+
 }
