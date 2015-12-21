97c7a6683ad087a0aa9a5b69492f3bf76042473b
Jan Jorissen <janjorissen@gmail.com>
2015-12-21 15:48:09 +0100

    URL change

--- utils/saveAnalystRatings.sh
+++ utils/saveAnalystRatings.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST -H "Content-Type: application/json" -H "tickers: ["TSLA"]" -H "Cache-Control: no-cache" -H "Postman-Token: d9ec2d5c-e17c-d7c2-ff64-50537bed23ef" -d '{"key":"top_secret_key_boy"}' 'http://localhost:3000/api/saveAnalystRatings';
+curl -X POST -H "Content-Type: application/json" -H "tickers: ["TSLA"]" -H "Cache-Control: no-cache" -H "Postman-Token: d9ec2d5c-e17c-d7c2-ff64-50537bed23ef" -d '{"key":"top_secret_key_boy"}' 'http://sixtyplus-test.herokuapp.com:3000/api/saveAnalystRatings';
