c633b5c713898ec7466feb29ffe7934293eb4171
Jan Jorissen <janjorissen@gmail.com>
2015-12-23 12:23:21 +0100

    todo updated

--- TODO.js
+++ TODO.js
@@ -1,5 +1,3 @@
-
-
  //OK Handle loading of realtime portfolio server side
  //OK Make seperate components for Quikstart / Stocks/ Import etc
  //OK Make seperate object for the state in Quickstart > Only save relevant items
@@ -16,8 +14,14 @@
  //OK Caching for everything
  //OK Cache the streaming prices and let the users 'fetch' it without making extra call to yahoo finance
  //OK Make filter and sorting components seperate re-usable components
- // Graph with total returns / dividends / costbase
- // Modal for stock entry information
+ //OK Graph with total returns / dividends / costbase
+ //OK Modal for stock entry information
+ //OK Run a cron job for getting daily data for all tickers in the database
+ //OK Fix the news module (proper loading of data through actions)
+ //OK Add node environment vars for making a distinction between different devs/test/debug accounts
+ //OK Integrate "fake" backend service for offline dev
+ //OK Store the analyst ratings over time so we can use the data to see what the trend is
+ //OK Draw the graph only with filtered data
  // Use immutable in the components
  // How many stocks make up how much of the loss / profit?
  // What is the best performing sector? Sectors?
@@ -27,14 +31,8 @@
  //// Time it takes for dividends to earn back loss
  //// Time it takes to earn back loss using data from your income
  // Write tests
- // Integrate "fake" backend service for offline dev
  // Use the same detailpage for stock entries as for your portfolio
- // Store the analyst ratings over time so we can use the data to see what the trend is
  // Notification for when analyst rating drops or divdends is cut/late
  // filtering by good / bad news for news
- // Draw the graph only with filtered data
  // Make the quickstart a real quickstart explaining the steps and doing the first import
  // Hover and explanations for the dividend table. Add sorting for the dividend table
- // Run a cron job for getting daily data for all tickers in the database
- // Fix the news module (proper loading of data through actions)
- // Add node environment vars for making a distinction between different devs/test/debug accounts
